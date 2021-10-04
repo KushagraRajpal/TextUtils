c0a8e948c2c3b0eada2ced96d205c37bf98d3179
Kushagra <kushagrarajpal23@gmail.com>
2021-10-04 16:30:51 +0530

    video-7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+
 function App() {
   return (
     <>
       <Navbar title= "TextUtils" aboutText = "about TextUtils"/>
+      <div className="container my-3">
+
+      <TextForm heading = "Enter the text to analyze below" />
+      </div>
     </>
   );
 }
