794af3e3f74e9cbb82292f6f5e7cf28f6505ea97
Kushagra <kushagrarajpal23@gmail.com>
2021-10-05 12:59:37 +0530

    video9

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,10 +12,23 @@ export default function TextForm(props) {
         let newText  = text.toLowerCase();
         setText(newText);
     }
+    const handleClear = () => {
+        let newText  = '';
+        setText(newText);
+    }
+    const handleSpace = () => {
+
+        let newText  = text.charAt(0).toUpperCase()+text.slice(1).toLowerCase()
+      // let newText = text.replace(/\w\S*/g + cap)
+          // console.log(cap,newText)
+
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         // console.log('Upper case clicked');
         setText(event.target.value)
     }
+    
     const [text, setText] = useState('');
     //   text = "new text" // Wrong way to change the state
         // setText("new text") // correct way to change the state
@@ -33,7 +46,10 @@ export default function TextForm(props) {
         ></textarea>
       </div>
       <button className="btn btn-primary m-3" onClick = {handleUpClick}>Convert to uppercase</button>
-      <button className="btn btn-primary" onClick = {handleLowClick}>Convert to lowercase</button>
+      <button className="btn btn-primary m-3" onClick = {handleLowClick}>Convert to lowercase</button>
+      <button className="btn btn-primary m-3" onClick = {handleClear}>Clear Text</button>
+      <button className="btn btn-primary m-3" onClick = {handleSpace}>Sentence Case</button>
+      
     </div>
     <div className="container my-3">
       <h2>Your text summary</h2>
