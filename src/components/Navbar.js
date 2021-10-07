65a7b2a53a2f6ce821bf6f3a113143f52d9611e2
KushagraRajpal <kushagrarajpal23@gmail.com>
2021-10-07 15:51:44 +0530

    video 20

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,15 +36,22 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
+          <div className="d-flex">
+            <div className="bg-primary rounded mx-2" onClick= {()=>{props.toggleMode('primary')}} style = {{height: '30px', width:'30px', curser:'pointer'}}></div>
+            <div className="bg-danger rounded mx-2" onClick= {()=>{props.toggleMode('danger')}} style = {{height: '30px', width:'30px', curser:'pointer'}}></div>
+            <div className="bg-success rounded mx-2" onClick= {()=>{props.toggleMode('success')}} style = {{height: '30px', width:'30px', curser:'pointer'}}></div>
+            <div className="bg-warning rounded mx-2" onClick= {()=>{props.toggleMode('warning')}} style = {{height: '30px', width:'30px', curser:'pointer'}}></div>
+
+          </div>
           <div className={`form-check form-switch text-${props.mode === 'light' ? "dark" : "light"} m-3`}>
             <input
               className="form-check-input"
-              onClick={props.toggleMode}
+              onClick= {()=>{props.toggleMode(null)}}
               type="checkbox"
               id="flexSwitchCheckDefault"
             />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-              Enable Dark Mode
+              Toogle Mode
             </label>
           </div>
 
